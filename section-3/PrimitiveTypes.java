1bfcab4877875240fb3d142c71da2cdc6a145a2a
Nathan Pham <nathanpham.me@gmail.com>
2021-06-04 22:33:58 -0700

    hello, variables, types

---
+++ section-3/PrimitiveTypes.java
@@ -0,0 +1,6 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        System.out.println("Minimum Integer: " + Integer.MIN_VALUE);
+        System.out.println("Maximum Integer: " + Integer.MAX_VALUE);
+    }
+}
---
+++ section-3/Variables.java
@@ -0,0 +1,7 @@
+public class Variables {
+    public static void main(String[] args) {
+        int number = 5;
+
+        System.out.println(number);
+    }
+}
