17e0dadf2511528bc0ea37d4a415c0241699cd45
Nathan Pham <nathanpham.me@gmail.com>
2021-06-04 22:51:10 -0700

    expanding on switches

--- section-5/Switch.java
+++ section-5/Switch.java
@@ -2,19 +2,19 @@ public class Switch {
     public static void main(String[] args) {
         String exampleString = "Hello World";
 
-        switch(exampleString) {
-            case "Hello World":
-                System.out.println("exampleString is Hello World");
+        switch(exampleString.toLowerCase()) {
+            case "hello world":
+                System.out.println("exampleString is " + exampleString);
                 break;
 
-            case "Bob":
-            case "Joe":
-            case "Nic":
-                System.out.println("exampleString was Bob, Joe, or Nic");
+            case "bob":
+            case "joe":
+            case "git":
+                System.out.println("exampleString was " + exampleString);
                 break;
 
             default:
-                System.out.println("exampleString does not fit the cases, it is " + exampleString);
+                System.out.println("exampleString does not fit the given cases");
                 break;
         }
     }
