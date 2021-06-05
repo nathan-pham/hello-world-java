3b04a45bf63e9fcb4527d622c44ffafb2b20c30a
Nathan Pham <nathanpham.me@gmail.com>
2021-06-04 23:01:43 -0700

    two solutions

--- section-5/DayChallenge.java
+++ section-5/DayChallenge.java
@@ -3,17 +3,37 @@ public class DayChallenge {
         System.out.println(getDayOfWeek(1));
         System.out.println(getDayOfWeek(2));
         System.out.println(getDayOfWeek(9));
+
+        System.out.println(getDayOfWeekArray(1));
+        System.out.println(getDayOfWeekArray(2));
+        System.out.println(getDayOfWeekArray(100));
+    }
+    
+    private static String getDayOfWeekArray(int day) {
+        String[] days = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thurday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        };
+
+        return day > 0 && day < days.length
+            ? days[day]
+            : "Invalid day of the week";
     }
 
     private static String getDayOfWeek(int day) {
         switch(day) {
-            case 1: return "Monday";
-            case 2: return "Tuesday";
-            case 3: return "Wednesday";
-            case 4: return "Thurday";
-            case 5: return "Friday";
-            case 6: return "Saturday";
-            case 7: return "Sunday";
+            case 0: return "Monday";
+            case 1: return "Tuesday";
+            case 2: return "Wednesday";
+            case 3: return "Thurday";
+            case 4: return "Friday";
+            case 5: return "Saturday";
+            case 6: return "Sunday";
             default: return "Invalid day of the week";
         }
     }
